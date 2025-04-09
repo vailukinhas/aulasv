37505754bf7f367a07a14658fab5ec801ce0b992
vailukinhas <tvlukinhastv@gmail.com>
2025-04-09 16:43:49 -0300

    Add files via upload

---
+++ www/www/aula_1/soma_variaveis.js
@@ -0,0 +1,4 @@
+//soma de 2 var de um numero
+var soma1 = 1
+var soma2 = 3
+console.log (soma1 + soma2 )
---
+++ www/www/aula_2/operador.js
@@ -0,0 +1,12 @@
+console.log(5 == 5);//true -           ==igualdade de valor
+console.log(5 != 5);//false -          !=diferenca de valor  ! não
+console.log(10 > 7)//true -            > maior
+console.log(3 < 5)//true -             < menor
+console.log(7 >= 7)//true -           >= maior ou =
+console.log(4 <= 9)//true -           <= menor ou =
+console.log(5 == 5 && 10 > 7)//true -  & e
+console.log(5 == 4 && 10 > 7)//false- 
+console.log(5 == 5 || 5 > 7)//true -  || ou 
+console.log(5 == 5 || 5 > 7)//true - 
+                                          
+//                                      ! nã0
